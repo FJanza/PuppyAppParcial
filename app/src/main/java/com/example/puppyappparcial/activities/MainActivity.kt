550d1e41e67897ba403d182fed82900bd2591c5d
CBossio <christianarielbossio@gmail.com>
2023-11-06 01:20:21 -0300

    <feat> agregar validacion al login

--- app/src/main/java/com/example/puppyappparcial/activities/MainActivity.kt
+++ app/src/main/java/com/example/puppyappparcial/activities/MainActivity.kt
@@ -1,48 +1,69 @@
-package com.example.puppyappparcial.activities
+package  com.example.puppyappparcial.activities
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.puppyappparcial.R
 import com.example.puppyappparcial.databinding.ActivityMainBinding
-import dagger.hilt.android.AndroidEntryPoint
-import java.util.concurrent.Executors
 
-@AndroidEntryPoint
+
 class MainActivity : AppCompatActivity() {
 
-    lateinit var submit: Button
-    lateinit var txtNombre: EditText
-    lateinit var txtImagen: EditText
-    lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-    override fun onStart() {
-        super.onStart()
+    private var submit_text: Button? = null
+    private var nombre_text: EditText? = null
+    private var numero_text: EditText? = null
+    private var imagen_text: EditText? = null
+    lateinit var binding_text: ActivityMainBinding
+    var isAllFieldsChecked = false
 
-        submit = binding.btnSubmit
-        txtImagen = binding.txtImagen
-        txtNombre = binding.txtNombre
-
-        submit.setOnClickListener {
-            val nombre = txtNombre.text.toString()
-            val imagenUrl = txtImagen.text.toString()
-            val intent = Intent(this, MainActivity2::class.java)
-            intent.putExtra("nombre", nombre)
-            intent.putExtra("imagenUrl", imagenUrl)
-            startActivity(intent)
-            finish()
+    private fun checkAllFields(): Boolean {
+        if (nombre_text?.text.isNullOrBlank()) {
+            nombre_text?.error = "Ingrese un nombre valido"
+            return false
         }
 
+        if (numero_text?.text.isNullOrBlank()) {
+            numero_text?.error = "Ingrese un numero valido"
+            return false
+        }
+        if (imagen_text?.text.isNullOrBlank()) {
+            imagen_text?.error = "Ingrese una URL valida"
+            return false
+        }
+
+        return true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        submit_text = findViewById(R.id.btnSubmit)
+        nombre_text = findViewById(R.id.txtNombre)
+        numero_text = findViewById(R.id.txtnumero)
+        imagen_text = findViewById(R.id.txtImagen)
+        
+
+        submit_text?.setOnClickListener {
+            isAllFieldsChecked = checkAllFields()
+
+            if (isAllFieldsChecked) {
+                val nombre = nombre_text.toString()
+                val imagenUrl = imagen_text.toString()
+                val intent = Intent(this@MainActivity, MainActivity2::class.java)
+                intent.putExtra("nombre", nombre)
+                intent.putExtra("imagenUrl", imagenUrl)
+                startActivity(intent)
+                finish()
+                startActivity(intent)
+            }
+        }
     }
 }
+
+
+
+
+
+
+
