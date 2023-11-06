aebf855d94a4b27d190457a5f6e5dd2938de4f20
CBossio <christianarielbossio@gmail.com>
2023-11-06 20:13:49 -0300

    <fix> arreglar perfil y errores login page

--- app/src/main/java/com/example/puppyappparcial/activities/MainActivity.kt
+++ app/src/main/java/com/example/puppyappparcial/activities/MainActivity.kt
@@ -1,69 +1,64 @@
-package  com.example.puppyappparcial.activities
+package com.example.puppyappparcial.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.puppyappparcial.R
 import com.example.puppyappparcial.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
-    private var submit_text: Button? = null
-    private var nombre_text: EditText? = null
-    private var numero_text: EditText? = null
-    private var imagen_text: EditText? = null
-    lateinit var binding_text: ActivityMainBinding
+    lateinit var submit: Button
+    lateinit var txtNombre: EditText
+    lateinit var txtImagen: EditText
+    lateinit var txtNumero: EditText
+    lateinit var binding: ActivityMainBinding
     var isAllFieldsChecked = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        submit = binding.btnSubmit
+        txtImagen = binding.txtImagen
+        txtNombre = binding.txtNombre
+        txtNumero = binding.txtNumero
+
+        submit.setOnClickListener {
+            isAllFieldsChecked = checkAllFields()
+            if (isAllFieldsChecked == true) {
+                val nombre = txtNombre.text.toString()
+                val imagenUrl = txtImagen.text.toString()
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("nombre", nombre)
+                intent.putExtra("imagenUrl", imagenUrl)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+
+    }
 
     private fun checkAllFields(): Boolean {
-        if (nombre_text?.text.isNullOrBlank()) {
-            nombre_text?.error = "Ingrese un nombre valido"
+        if (txtNombre.text.toString().isNullOrBlank()) {
+            txtNombre.error = "Ingrese un nombre valido"
             return false
         }
-
-        if (numero_text?.text.isNullOrBlank()) {
-            numero_text?.error = "Ingrese un numero valido"
+        if (txtNumero.text.toString().isNullOrBlank() || txtNumero.text.length < 8 ) {
+            txtNumero.error = "Ingrese un numero valido"
             return false
         }
-        if (imagen_text?.text.isNullOrBlank()) {
-            imagen_text?.error = "Ingrese una URL valida"
+        if (txtImagen.text.toString().isNullOrBlank()) {
+            txtImagen.error = "Ingrese un nombre valido"
             return false
         }
 
         return true
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        submit_text = findViewById(R.id.btnSubmit)
-        nombre_text = findViewById(R.id.txtNombre)
-        numero_text = findViewById(R.id.txtnumero)
-        imagen_text = findViewById(R.id.txtImagen)
-        
-
-        submit_text?.setOnClickListener {
-            isAllFieldsChecked = checkAllFields()
-
-            if (isAllFieldsChecked) {
-                val nombre = nombre_text.toString()
-                val imagenUrl = imagen_text.toString()
-                val intent = Intent(this@MainActivity, MainActivity2::class.java)
-                intent.putExtra("nombre", nombre)
-                intent.putExtra("imagenUrl", imagenUrl)
-                startActivity(intent)
-                finish()
-                startActivity(intent)
-            }
-        }
-    }
 }
-
-
-
-
-
-
-
--- app/src/main/java/com/example/puppyappparcial/fragments/Profile.kt
+++ app/src/main/java/com/example/puppyappparcial/fragments/Profile.kt
@@ -1,17 +1,17 @@
 package com.example.puppyappparcial.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.URLUtil
 import android.widget.Button
 import android.widget.EditText
-import com.bumptech.glide.Glide
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.puppyappparcial.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -58,6 +58,7 @@ class Profile : Fragment() {
         }
 
         if (!imagenUrl.isNullOrEmpty()) {
+
             Glide.with(requireContext())
                 .load(imagenUrl)
                 .into(imageView)
