1248dd125429ed8f4b5765f58ad45e3f8d409842
Matias Javier Gutierrez <matias.j.gutierrez@accenture.com>
2023-11-07 16:37:09 -0300

    <fix> Actualizar Data Base

--- app/src/main/java/com/example/puppyappparcial/data/database/PublicationDataBase.kt
+++ app/src/main/java/com/example/puppyappparcial/data/database/PublicationDataBase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.puppyappparcial.data.database.dao.PublicationDao
 import com.example.puppyappparcial.data.database.entities.PublicationEntity
 
-@Database(entities = [PublicationEntity::class], version=2)
+@Database(entities = [PublicationEntity::class], version=3)
 abstract class PublicationDataBase: RoomDatabase() {
     abstract fun getPublicationDao () : PublicationDao
 }
