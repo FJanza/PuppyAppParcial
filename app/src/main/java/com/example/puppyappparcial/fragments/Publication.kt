f170333272a848abbda26702bf6577af1dd2daad
CBossio <christianarielbossio@gmail.com>
2023-11-06 21:07:18 -0300

    <feat> validar inputs de nueva publicacion

--- app/src/main/java/com/example/puppyappparcial/fragments/Publication.kt
+++ app/src/main/java/com/example/puppyappparcial/fragments/Publication.kt
@@ -1,60 +1,93 @@
 package com.example.puppyappparcial.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
 import com.example.puppyappparcial.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Publication.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Publication : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var buttonUpload: Button
+    lateinit var inputName: EditText
+    lateinit var inputAge: EditText
+    lateinit var inputLocation: EditText
+    lateinit var inputGender: EditText
+    lateinit var inputWeigth: EditText
+    lateinit var inputDescription: EditText
+    lateinit var inputImageUrl: EditText
+    lateinit var inputImageUrl2: EditText
+    lateinit var inputImageUrl3: EditText
+    var isAllFieldsChecked = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_publication, container, false)
+        val view = inflater.inflate(R.layout.fragment_publication, container, false)
+        super.onViewCreated(view, savedInstanceState)
+        inputName = view.findViewById(R.id.inputName)
+        inputAge = view.findViewById(R.id.inputAge)
+        inputLocation = view.findViewById(R.id.inputLocation)
+        inputGender = view.findViewById(R.id.inputGender)
+        inputWeigth = view.findViewById(R.id.inputWeigth)
+        inputDescription = view.findViewById(R.id.inputDescription)
+        inputImageUrl = view.findViewById(R.id.inputImageUrl)
+        inputImageUrl2 = view.findViewById(R.id.inputImageUrl2)
+        inputImageUrl3 = view.findViewById(R.id.inputImageUrl3)
+        buttonUpload = view.findViewById(R.id.buttonUpload)
+
+        buttonUpload.setOnClickListener {
+            isAllFieldsChecked = checkAllFields()
+            if (isAllFieldsChecked == true) {
+               //AGREGAR METODO QUE AGREGUE EL PERRO A LA BASE DE DATOS
+            } 
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Publication.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Publication().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun checkAllFields(): Boolean {
+        if (inputName.text.toString().isNullOrBlank()) {
+            inputName.error = "Ingrese un nombre valido"
+            return false
+        }
+        if (inputAge.text.toString().isNullOrBlank()) {
+            inputAge.error = "Ingrese una edad valida"
+            return false
+        }
+        if (inputLocation.text.toString().isNullOrBlank()) {
+            inputLocation.error = "Ingrese una ubicacion valida"
+            return false
+        }
+        if (inputGender.text.toString().isNullOrBlank()) {
+            inputGender.error = "Ingrese un genero valido"
+            return false
+        }
+        if (inputWeigth.text.toString().isNullOrBlank()) {
+            inputWeigth.error = "Ingrese un peso valido"
+            return false
+        }
+        if (inputDescription.text.toString().isNullOrBlank()) {
+            inputDescription.error = "Ingrese una descripcion valida"
+            return false
+        }
+        if (inputImageUrl.text.toString().isNullOrBlank()) {
+            inputImageUrl.error = "Ingrese una imagen valida"
+            return false
+        }
+        if (inputImageUrl2.text.toString().isNullOrBlank()) {
+            inputImageUrl2.error = "Ingrese una imagen valida"
+            return false
+        }
+        if (inputImageUrl3.text.toString().isNullOrBlank()) {
+            inputImageUrl3.error = "Ingrese una imagen valida"
+            return false
+        }
+
+        return true
     }
+
 }
